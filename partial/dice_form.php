3a9741266fe58a6352ba3d82ce29aedabad362f0
GFROCRAI <gwenda.frocrain@gmail.com>
2018-01-21 03:58:37 +0100

    del

--- partial/dice_form.php
+++
@@ -1,7 +0,0 @@
-<form action="roll_dice.php" method="POST">
-	<input type="hidden" name="action" value="<?php echo $action ?>">
-	<input type="hidden" name="name" value="<?php echo $_SESSION['up_to'] ?>">
-	<div class="flex-center">
-		<input name="dice" value="Roll" type="submit" <?php if (!$_SESSION['pp_set']) echo "disabled"; ?>/>
-	</div>
-</form>
